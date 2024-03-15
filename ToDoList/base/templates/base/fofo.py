7f195a981948dd6c4ba6b6359ccab285d4e4a77d
OlivierCodes <134596419+OlivierCodes@users.noreply.github.com>
2024-03-15 14:35:19 +0000

    Add files via upload

---
+++ ToDoList/base/templates/base/fofo.py
@@ -0,0 +1,21 @@
+
+
+
+print("----------Bienvenue dans ce rubrique de table de multiplication---------- ")
+
+n = int(input("Entrez le numero de la table:"))
+
+
+for  i in range (0, 11):
+    result = i*n
+    print(str(i) + "*" + str(n) + " = " + str(result))
+
+
+
+
+
+
+
+
+
+
